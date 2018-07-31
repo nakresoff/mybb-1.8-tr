db888c8434de79f51c7e3498647e993498e2149d
Medet ERDAL <35021963+tedem@users.noreply.github.com>
2018-07-31 13:14:42 +0300

    Update archive.lang.php

--- inc/languages/turkish/archive.lang.php
+++ inc/languages/turkish/archive.lang.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Turkish Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['archive_fullversion'] = "Full Version:";
-$l['archive_replies'] = "Replies";
-$l['archive_reply'] = "Reply";
-$l['archive_pages'] = "Pages:";
-$l['archive_note'] = "You're currently viewing a stripped down version of our content. <a href=\"{1}\">View the full version</a> with proper formatting.";
-$l['archive_nopermission'] = "Sorry, you do not have permission to access this resource.";
-$l['error_nothreads'] = "There are currently no threads in this forum.";
-$l['error_unapproved_thread'] = "This thread is unapproved. Please <a href=\"{1}\">view the full version</a> to view the contents of this thread.";
-$l['archive_not_found'] = "The requested page was not found on this server.";
-$l['error_mustlogin'] = "This bulletin board requires all users to login.";
+$l['archive_fullversion'] = "Tam Versiyon:";
+$l['archive_replies'] = "Cevaplar";
+$l['archive_reply'] = "Cevap";
+$l['archive_pages'] = "Sayfalar:";
+$l['archive_note'] = "Hafifletilmiş bir versiyonunu inceliyorsunuz. <a href=\"{1}\">Tam versiyon</a> formatına geçin.";
+$l['archive_nopermission'] = "Üzgünüz, bu kaynağa erişim izniniz yok.";
+$l['error_nothreads'] = "Şu anda bu forumda konu yok.";
+$l['error_unapproved_thread'] = "Bu konu onaylanmadı. Bu konunun içeriğini görüntüleyebilmek için <a href=\"{1}\">tam versiyon</a> formatına geçin.";
+$l['archive_not_found'] = "İstenen sayfa bu sunucuda bulunamadı.";
+$l['error_mustlogin'] = "Forumu görüntüleyebilmek için giriş yapmanız gerekiyor.";
